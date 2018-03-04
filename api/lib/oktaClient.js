89d712200f06f187ee322e854e42a15647f32d1c
lebeerman <omnidbeerman@gmail.com>
2018-03-03 18:09:23 -0700

    prepped for heroku deploy, routes working/though split between router and root

--- api/lib/oktaClient.js
+++ api/lib/oktaClient.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const okta = require('@okta/okta-sdk-nodejs');
 const client = new okta.Client({
   orgUrl: 'https://dev-375655.oktapreview.com',
-  token: '002ivEToJGZDN7hnlQSyWOAz-3-mbo1dZ4F0chhVQA'
+  token: process.env.OKTA_TOKEN
 });
 
 module.exports = client;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const queries = require('../queries');
 const oktaClient = require('../lib/oktaClient');
-
 /* Create a new User (register). */
 router.post('/', (req, res, next) => {
   console.log('Made it to the user routes:',req.body);
@@ -32,6 +32,39 @@ router.post('/', (req, res, next) => {
     //   // console.log(Object.keys(err))
     //   res.send({message: err.message, stack: err.stack});
     // });
+});
+// Middleware --- all the routing needed for CRUD funcitonality!
+// List all the Goals
+router.get("/db", (request, response) => {
+    queries.list().then(goals => {
+        response.json({goals});
+    }).catch(console.error);
+});
+// List a single goal as response
+router.get("/db/:id", (request, response) => {
+    queries.read(request.params.id).then(goals => {
+        goals
+            ? response.json({goals})
+            : response.sendStatus(404)
+    }).catch(console.error);
+});
+// Add a goal to goals database
+router.post("/db", (request, response) => {
+    queries.create(request.body).then(goal => {
+        response.status(201).json({goals: goal});
+    }).catch(console.error);
+});
+// remove a goal by ID from the database
+router.delete("/db/:id", (request, response) => {
+    queries.delete(request.params.id).then(() => {
+        response.sendStatus(204);
+    }).catch(console.error);
+});
+// update a goal in the database
+router.put("/db/:id", (request, response) => {
+    queries.update(request.params.id, request.body).then(resolution => {
+        response.json({goal: resolution[0]});
+    }).catch(console.error);
 });
 
 module.exports = router;
